b0ba4fd5fae27e6eae9b1adcd1db239f9bd6f3db
Alexis-ROBERT <alexisrobert.github@gmail.com>
2023-11-27 07:34:01 +0100

    Modified of controller

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,32 +1,39 @@
-import { Body, Controller, Delete, Get, Patch, Post, Req, Res } from '@nestjs/common';
-import UserService from './user.service';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 import UserCreateDto from './dto/user.create.dto';
 import UserFindDto from './dto/user.find.dto';
 import UserFindAllDto from './dto/user.find.all';
 import UserDeleteDto from './dto/user.delete.dto';
 import UserChangeDto from './dto/user.change.dto';
+import UserService from './user.service';
+import UserEntity from './user.entity';
 
 @Controller('user')
 export default class UserController {
-        public constructor(
-                public user: UserService,
-                @Req() public req: Request,
-                @Res() public res: Response
-        ) {}
+        public constructor(public user: UserService) {}
 
         @Post()
-        public userCreate(@Body() userCreateDto: UserCreateDto) {}
+        public async userCreate(@Body() userCreateDto: UserCreateDto): Promise<UserEntity> {
+                try {
+                        return await this.user.create(userCreateDto);
+                } catch (err) {
+                        let httpError: HttpException;
 
-        @Get()
-        public userFind(@Body() userFindDto: UserFindDto) {}
+                        if (err instanceof HttpException) {
+                                httpError = err as HttpException;
+                        } else httpError = new HttpException('Server error internal', HttpStatus.INTERNAL_SERVER_ERROR);
+
+                        throw new HttpException(httpError.cause, httpError.getStatus());
+                }
+        }
+
+        @Get(':id')
+        public userFind(@Body() userFindDto: UserFindDto, @Param(':id') userId: number) {}
 
         @Get()
         public userFindAll(@Body() userFindAllDto: UserFindAllDto) {}
 
         @Patch()
-        public userChange(@Body() userChangeDto: UserChangeDto) {
-            
-        }
+        public userChange(@Body() userChangeDto: UserChangeDto) {}
 
         @Delete()
         public userDelete(@Body() userDelete: UserDeleteDto) {}
