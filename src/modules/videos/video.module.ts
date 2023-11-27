8fbc135fba5996d8b530ff9829860e8f76313c72
Alexis-ROBERT <alexisrobert.github@gmail.com>
2023-11-27 07:36:24 +0100

    Modified modules

--- src/modules/videos/video.module.ts
+++ src/modules/videos/video.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
 import VideoService from "./video.service";
+import VideoController from "./video.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import videoSchema from "./video.schema";
 
 @Module({
-        controllers: [VideoService],
+        imports: [MongooseModule.forFeature([{name: 'video_data', schema: videoSchema}])],
+        controllers: [VideoController],
         providers: []
 })
 export default class VideoModule {}
