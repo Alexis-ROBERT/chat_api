61e0d986e838671c5664e99261f4b5bf7e7d7ee5
Alexis-ROBERT <alexisrobert.github@gmail.com>
2023-11-27 07:30:01 +0100

    Modified module

--- src/modules/history/history.module.ts
+++ src/modules/history/history.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import HistoryController from './history.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { historySchema } from './history.schema';
+import HistoryService from './history.service';
+import HistoryRepository from './history.repository';
 
-@Module({})
+@Module({
+        imports: [MongooseModule.forFeature([{ name: 'history_data', schema: historySchema }])],
+        controllers: [HistoryController],
+        providers: [HistoryService, HistoryRepository],
+})
 export default class HistoryModule {}
