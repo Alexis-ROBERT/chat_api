20618256bfc18ee06f8fb55c821560a8612381cc
Alexis-ROBERT <128067166+Alexis-ROBERT@users.noreply.github.com>
2023-12-03 18:38:03 +0100

    Added of a multiple verification of datas

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import UserRepository from './user.repository';
-import { UserDocument } from './user.type';
+import { IUserService, UserDocument } from './user.type';
 import UserCreateDto from './dto/user.create.dto';
 import * as bcrypt from 'bcrypt';
 import UserEntity from './user.entity';
@@ -8,36 +8,48 @@ import * as crypto from 'crypto';
 import Crypto from 'src/utils/crypto';
 
 @Injectable()
-export default class UserService {
+export default class UserService implements IUserService {
         public constructor(public readonly userReposetory: UserRepository) {}
 
         public async create(userCreateDto: UserCreateDto): Promise<UserEntity> {
-                const crypto: Crypto = new Crypto('aes-256-gcm', process.env.CRYPTO_SECRET_KEY, 16);
-
                 userCreateDto.email = userCreateDto.email.toLowerCase();
-                userCreateDto.email = (await crypto.crypt(userCreateDto.email)).result;
 
                 if (
                         !RegExp(process.env.REGEX_MAIL as RegExp | string).test(userCreateDto.email) ||
                         !RegExp(process.env.REGEX_PASSWORD as RegExp | string).test(userCreateDto.password)
-                ) {
+                )
                         throw new HttpException('The regex', HttpStatus.NOT_ACCEPTABLE);
+
+                userCreateDto.password = bcrypt.hashSync(userCreateDto.password, 10);
+
+                const verify: {} = {
+                        email: userCreateDto.email,
+                        username: userCreateDto.username,
+                };
+
+                for (const [key, value] of Object.entries(verify)) {
+                        await this.userReposetory
+                                .find({
+                                        [key]: value,
+                                })
+                                .then((find) => {
+                                        if (find.length > 0) {
+                                                throw new HttpException(
+                                                        key.split(', ').map((k, i) => {
+                                                                console.log(i);
+                                                                if (i === 0) return k.toLocaleUpperCase();
+                                                                return k;
+                                                        }) + ' already exists.',
+                                                        HttpStatus.FOUND
+                                                );
+                                        }
+
+                                        console.log('s')
+                                })
+                                .catch((err: string) => {
+                                        throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+                                });
                 }
-
-                userCreateDto.password = bcrypt.hashSync(userCreateDto.password, 12);
-
-                await this.userReposetory
-                        .find({
-                                email: userCreateDto.email,
-                        })
-                        .then((find) => {
-                                if (find.length > 0) {
-                                        throw new HttpException('Email already exists', HttpStatus.FOUND);
-                                }
-                        })
-                        .catch((err: string) => {
-                                throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
-                        });
 
                 if (userCreateDto.age <= 14) throw new HttpException('', HttpStatus.NOT_ACCEPTABLE);
 
